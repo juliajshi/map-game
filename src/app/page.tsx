cfdebcbcfe690d74a5838b20b82e45954182d7cf
raveen <raveenreddy89@gmail.com>
2025-04-08 12:46:50 -0400

    latest and greatest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,80 @@
-import Image from "next/image";
+"use client";
+
+import { Feature, LineString } from "geojson";
+import React, { useState } from "react";
+import Map, { Layer, Marker, Source, ViewState } from "react-map-gl/mapbox";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const initialViewState = {
+  longitude: -122.4,
+  latitude: 37.8,
+  zoom: 14,
+  bearing: 0,
+  pitch: 80,
+  padding: { top: 0, bottom: 0, left: 0, right: 0 },
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [viewState, setViewState] = useState<ViewState>(initialViewState);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  console.log("viewState zoom", viewState.zoom);
+
+  const samplePath: Feature<LineString> = {
+    type: "Feature",
+    properties: {
+      name: "Fisherman's Wharf to Golden Gate Bridge",
+      description: "Scenic path along San Francisco's northern waterfront",
+    },
+    geometry: {
+      type: "LineString",
+      coordinates: [
+        [-122.417482, 37.808414], // Fisherman's Wharf
+        [-122.422481, 37.807207], // Aquatic Park
+        [-122.425873, 37.805872], // Fort Mason
+        [-122.431435, 37.805958], // Marina Green
+        [-122.43799, 37.80653], // Palace of Fine Arts
+        [-122.443736, 37.803883], // Crissy Field East
+        [-122.456333, 37.802029], // Crissy Field West
+        [-122.466429, 37.80438], // Fort Point
+        [-122.478417, 37.811758], // Golden Gate Bridge Vista Point
+      ],
+    },
+  };
+
+  return (
+    <div>
+      <Map
+        mapboxAccessToken="pk.eyJ1IjoiaWFtbm90cmVkZHkiLCJhIjoiY2w3amhyOTNmMHlhdzN1cG9sNGptbzhueSJ9.1F6iaOfrxBwF1174T3PX5A"
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        onMove={(evt) => setViewState(evt.viewState)}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/iamnotreddy/cm98oiazk002701s0b0j68mpr"
+        maxPitch={85}
+        minPitch={80}
+        minZoom={20}
+        maxZoom={21}
+      >
+        <Source id="sample-path" type="geojson" data={samplePath}>
+          <Layer
+            id="path-layer"
+            type="line"
+            paint={{
+              "line-color": "#0080ff",
+              "line-width": 3,
+            }}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </Source>
+        <Marker
+          latitude={viewState.latitude}
+          longitude={viewState.longitude}
+        ></Marker>
+      </Map>
     </div>
   );
 }
+
+//
