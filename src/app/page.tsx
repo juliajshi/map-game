1a6919cee269459430ba97cf2ee6bb8d4096e665
Julia Shi <juliajshi@gmail.com>
2025-04-08 16:38:43 -0400

    added marker and route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { Feature, LineString, Point } from "geojson";
-import React, { useCallback, useEffect, useState } from "react";
-import Map, { Layer, Marker, Source, ViewState } from "react-map-gl/mapbox";
-import "mapbox-gl/dist/mapbox-gl.css";
+import { Feature, LineString, Point } from 'geojson';
+import React, { useCallback, useEffect, useState } from 'react';
+import Map, { Layer, Marker, Source, ViewState } from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const initialViewState = {
   longitude: -73.9853326880174, // NYC longitude
@@ -15,13 +15,13 @@ const initialViewState = {
 };
 
 const initialGamePath: Feature<LineString> = {
-  type: "Feature",
+  type: 'Feature',
   properties: {
     name: "Fisherman's Wharf to Golden Gate Bridge",
     description: "Scenic path along San Francisco's northern waterfront",
   },
   geometry: {
-    type: "LineString",
+    type: 'LineString',
     coordinates: [
       [-73.9853326880174, 40.6913874260183], // Recurse Center
       [-73.9953326880174, 40.7013874260183], // Recurse Center
@@ -29,17 +29,32 @@ const initialGamePath: Feature<LineString> = {
   },
 };
 
+const test: number[][] = [
+  [-73.9853326880174, 40.6913874260183], // Recurse Center
+  [-73.9953326880174, 40.7013874260183], // Recurse Center
+];
+
+const distance = (pt_prev: number[], pt_next: number[]) => {
+  const d_x = pt_next[0] - pt_prev[0];
+  const d_y = pt_next[1] - pt_prev[1];
+  return Math.sqrt(d_x * d_x + d_y * d_y);
+};
+
 export default function Home() {
   const [viewState, setViewState] = useState<ViewState>(initialViewState);
+  // const [currentCoordinates, setCurrentCoodinates] = useState([]);
+  const [pathCoordinates, setPathCoordinates] = useState([
+    [-73.9853326880174, 40.6913874260183], // Recurse Center
+  ]);
 
   const [gamePath, setGamePath] = useState<Feature<LineString>>({
-    type: "Feature",
+    type: 'Feature',
     properties: {
       name: "Fisherman's Wharf to Golden Gate Bridge",
       description: "Scenic path along San Francisco's northern waterfront",
     },
     geometry: {
-      type: "LineString",
+      type: 'LineString',
       coordinates: [
         [-73.9853326880174, 40.6913874260183], // Recurse Center
         [-73.9953326880174, 40.7013874260183], // Recurse Center
@@ -47,41 +62,54 @@ export default function Home() {
     },
   });
 
+  const returnGamePath = (input: number[][]): Feature<LineString> => {
+    return {
+      type: 'Feature',
+      properties: {
+        name: "Fisherman's Wharf to Golden Gate Bridge",
+        description: "Scenic path along San Francisco's northern waterfront",
+      },
+      geometry: {
+        type: 'LineString',
+        coordinates: input,
+      },
+    };
+  };
+
   const startPoint: Feature<Point> = {
-    type: "Feature",
+    type: 'Feature',
     properties: {
       name: "Fisherman's Wharf to Golden Gate Bridge",
       description: "Scenic path along San Francisco's northern waterfront",
     },
     geometry: {
-      type: "Point",
+      type: 'Point',
       coordinates: [-73.9853326880174, 40.6913874260183], // Fisherman's Wharf
     },
   };
 
   const initialGamePath: Feature<LineString> = {
-    type: "Feature",
+    type: 'Feature',
     properties: {
       name: "Fisherman's Wharf to Golden Gate Bridge",
       description: "Scenic path along San Francisco's northern waterfront",
     },
     geometry: {
-      type: "LineString",
+      type: 'LineString',
       coordinates: [
         [-73.9853326880174, 40.6913874260183], // Recurse Center
-        [-73.9953326880174, 40.7013874260183],
       ],
     },
   };
 
   const samplePath: Feature<LineString> = {
-    type: "Feature",
+    type: 'Feature',
     properties: {
       name: "Fisherman's Wharf to Golden Gate Bridge",
       description: "Scenic path along San Francisco's northern waterfront",
     },
     geometry: {
-      type: "LineString",
+      type: 'LineString',
       coordinates: [
         [-122.417482, 37.808414], // Fisherman's Wharf
         [-122.422481, 37.807207], // Aquatic Park
@@ -97,7 +125,7 @@ export default function Home() {
   };
 
   const handleKeyDown = useCallback((event: KeyboardEvent) => {
-    if (["q", "w"].includes(event.key.toLowerCase())) {
+    if (['q', 'w'].includes(event.key.toLowerCase())) {
       event.preventDefault();
 
       // Update bearing based on Q and W keys
@@ -105,9 +133,9 @@ export default function Home() {
         const bearingChange = 5; // degrees to rotate per key press
         let newBearing = prev.bearing;
 
-        if (event.key.toLowerCase() === "q") {
+        if (event.key.toLowerCase() === 'q') {
           newBearing = (prev.bearing - bearingChange) % 360;
-        } else if (event.key.toLowerCase() === "w") {
+        } else if (event.key.toLowerCase() === 'w') {
           newBearing = (prev.bearing + bearingChange) % 360;
         }
 
@@ -120,13 +148,15 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener('keydown', handleKeyDown);
 
     // Clean up event listener when component unmounts
     return () => {
-      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     };
   }, [handleKeyDown]);
+
+  console.log('path coordinates', pathCoordinates);
 
   return (
     <div>
@@ -139,31 +169,49 @@ export default function Home() {
         }}
         {...viewState}
         onMove={(evt) => {
-          console.log("we are moving to:", evt);
+          console.log('we are moving to:', evt);
+
+          const lastCoordinate = pathCoordinates[pathCoordinates.length - 1];
+          const nextCoordinate = [
+            evt.viewState.longitude,
+            evt.viewState.latitude,
+          ];
+
+          const lastDistance = distance(lastCoordinate, nextCoordinate);
+          console.log('last distance', lastDistance);
+          if (lastDistance > 0.00005) {
+            console.log('in the distance checker');
+            setPathCoordinates((prev) => [...prev, nextCoordinate]);
+          }
+          evt.viewState.latitude;
           setViewState(evt.viewState);
         }}
-        style={{ width: "100vw", height: "100vh" }}
-        mapStyle="mapbox://styles/iamnotreddy/cm98oiazk002701s0b0j68mpr"
+        style={{ width: '100vw', height: '100vh' }}
+        mapStyle='mapbox://styles/iamnotreddy/cm98oiazk002701s0b0j68mpr'
         maxPitch={85}
         minPitch={80}
         minZoom={0}
         maxZoom={22.5}
       >
-        <Source id="game-path" type="geojson" data={gamePath}>
+        <Source
+          id='game-path'
+          type='geojson'
+          data={returnGamePath(pathCoordinates)}
+        >
           <Layer
-            id="path-layer"
-            type="line"
+            id='path-layer'
+            type='line'
             paint={{
-              "line-color": "pink",
-              "line-width": 10,
+              'line-color': 'pink',
+              'line-width': 10,
             }}
           />
         </Source>
-        <Source id="start-point" type="geojson" data={startPoint}>
+        <Source id='start-point' type='geojson' data={startPoint}>
           <Layer
-            id="start-point-layer"
-            type="circle"
-            paint={{ "circle-color": "red" }}
+            id='start-point-layer'
+            type='circle'
+            paint={{ 'circle-color': 'red' }}
           />
         </Source>
         <Marker
